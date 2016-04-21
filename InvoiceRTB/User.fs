1839a3c15f2fe333b3550b1bf8823717796d5f23
jakecoltman <jakecoltman@sky.com>
2016-04-21 20:53:08 +0100

    Switched seller to user.  Current features are both buyer and seller

---
+++ InvoiceRTB/User.fs
@@ -0,0 +1,19 @@
+﻿namespace IRTB
+
+module User = 
+    
+    open IRTB.Payment
+
+    type User = {
+        name: string;
+        payments : Payment list
+    }
+
+    let add_payment (user: User) (payment: Payment) = 
+        {name = user.name; payments = List.append user.payments [payment]}
+
+    let create (name: string) : User = 
+        {name = name; payments = []}
+
+
+
