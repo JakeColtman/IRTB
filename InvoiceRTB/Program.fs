fb911f0eb0817c84fc3645c22c5208d9072d18e0
jakecoltman <jakecoltman@sky.com>
2016-04-21 21:46:17 +0100

    Added market starting

--- InvoiceRTB/Program.fs
+++ InvoiceRTB/Program.fs
@@ -1,7 +1,10 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
+open IRTB.Market
 
 [<EntryPoint>]
 let main argv = 
+    let market = IRTB.Market.market highest_bidder
+    market.Post "hello"
     printfn "%A" argv
     0 // return an integer exit code
