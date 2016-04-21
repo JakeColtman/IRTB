7fb6f9515ad89b8cc65e0555cbd7d41ea00ed34b
jakecoltman <jakecoltman@sky.com>
2016-04-21 20:51:17 +0100

    Added function to add new payments to seller

--- InvoiceRTB/Seller.fs
+++ InvoiceRTB/Seller.fs
@@ -9,6 +9,9 @@ module Seller =
         payments : Payment list
     }
 
+    let add_invoice (seller: Seller) (payment: Payment) = 
+        {name = seller.name; payments = List.append seller.payments [payment]}
+
     let create (name: string) : Seller = 
         {name = name; payments = []}
 
