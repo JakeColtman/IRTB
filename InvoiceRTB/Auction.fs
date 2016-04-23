6808851f7196a7fcf92232e78a582eb65a49ff81
jakecoltman <jakecoltman@sky.com>
2016-04-23 17:27:32 +0100

    Build out to enable bids on auctions

--- InvoiceRTB/Auction.fs
+++ InvoiceRTB/Auction.fs
@@ -14,9 +14,24 @@ module Auction =
     type Auction = {
         resolution : Resolution;
         auction_id: int;
-        bids: Bid list
+        bids: Bid list;
+        active: bool
     }
 
     let resolve_auction auction = 
         auction.resolution auction.bids
 
+    let is_auction_active (auction: Auction) = 
+        auction.bids.Length < 3
+
+    let bid_on_auction (auction: Auction) (bid: Bid) = 
+        let pre_state = is_auction_active auction
+        let updated_auction = 
+            match pre_state with 
+                | true -> {auction with bids = List.append auction.bids [bid]}
+                | false -> auction
+        let post_state = is_auction_active updated_auction
+        match post_state with 
+            | true -> updated_auction
+            | false -> {auction with active = false}
+            
